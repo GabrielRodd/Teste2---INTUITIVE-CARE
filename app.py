d5fbe9b2079460a1788893d63452f10adef0356e
GabrielRodd <gabriel.rdgs33@outlook.com>
2025-03-28 12:18:32 -0300

    Implementando melhorias no código
    Possibilidade de escolha do caminho do arquivo e retornos do processo para o usuário

--- app.py
+++ app.py
@@ -3,15 +3,42 @@ import pandas as pd
 import zipfile
 import os
 
-def compactar(arquivo,nome):
+
+def compactar(arquivo, nome):
     zip_path = (f'Teste_{nome}.zip')
     if os.path.exists(zip_path):
         os.remove(zip_path)
-    
-    with zipfile.ZipFile(zip_path,'w') as ziparquivo:
+
+    with zipfile.ZipFile(zip_path, 'w') as ziparquivo:
         ziparquivo.write(arquivo, os.path.basename(arquivo))
 
-caminho_arquivo = "AnexoPdf\\Anexo_1.pdf"
+
+print("Deseja colar o caminho do arquivo PDF ou usar o caminho padrão dessa pasta?")
+print("1 - Para usar caminho padrão da pasta")
+print("2 - Para colar novo caminho")
+print('\n')
+
+while True:
+    try:
+        opcao = int(input('1 ou 2:'))
+        if opcao == 1:
+            caminho_arquivo = ("AnexoPdf\\Anexo_1.pdf")
+            print('Prosseguindo com processo...')
+            print('\n')
+            break
+        
+        elif opcao == 2:
+            caminho = input('Cole o caminho do arquivo PDF desejado:')
+            caminho_arquivo = caminho.replace("\\", '\\\\')
+            print('Prosseguindo com processo...')
+            print('\n')
+            break
+        
+        else:
+            print("Digite apenas 1 ou 2")
+    
+    except ValueError:
+        print("Digite um valor NUMÉRICO entre 1 e 2")
 
 with pdfplumber.open(caminho_arquivo) as pdf:
     # Bloco de extração das tabelas do PDF(por páginas)
@@ -33,7 +60,7 @@ with pdfplumber.open(caminho_arquivo) as pdf:
         for tabela in (todas_tabelas[1:]):
             df = pd.DataFrame(tabela[1:]).fillna("")
             todos_dfs.append(df)
-        
+
         # Concatenando todos os data frames e gerando o arquivo csv
         df_final = pd.concat(todos_dfs)
         arquivo_csv = "TabelaAnexo1.csv"
@@ -41,14 +68,12 @@ with pdfplumber.open(caminho_arquivo) as pdf:
 
         if df_final is not None:
             print('Tabela estruturada em formato csv.')
-            compactar(arquivo_csv,"Gabriel_Rodrigues")
-        
+            compactar(arquivo_csv, "Gabriel_Rodrigues")
+
         # Bloco de edição das abreviações das colunas OD e AMB
         novo_df = pd.read_csv(arquivo_csv)
+        novo_df.rename(columns={'OD': 'Seg.Odontológica',
+                       'AMB': 'Seg.Ambulatorial'}, inplace=True)
+        novo_df.to_csv('Modificado.csv', index=False)
 
-        novo_df.rename(columns={'OD': 'Seg.Odontológica', 'AMB':'Seg.Ambulatorial'}, inplace=True)
-
-        novo_df.to_csv('Modificado.csv', index = False)
-
-        
-
+        print("Títulos das colunas OD e AMB alterados no arquivo 'Modificado.csv'")
